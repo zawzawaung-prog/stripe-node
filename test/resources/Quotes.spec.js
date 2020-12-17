716e841f7545b4728bdf656fe8aee6e546c6adb4
Richard Marmorstein <richardm@stripe.com>
2020-12-16 20:24:07 -0500

    A proof of concept

--- test/resources/Quotes.spec.js
+++ test/resources/Quotes.spec.js
@@ -1,19 +1,29 @@
 'use strict';
 
 const testUtils = require('../../testUtils');
+const fs = require('fs');
 
 describe('Quotes Resource', () => {
   it('pdf', (callback) => {
     const handleRequest = (req, res) => {
-      res.writeHeader(200);
-      res.write('some bytes');
-      res.end();
+      setTimeout(() => res.write('pretend'), 10);
+      setTimeout(() => res.write(' this'), 100);
+      setTimeout(() => res.write(' is a pdf'), 1000);
+      setTimeout(() => res.end(), 1200);
     };
+
     testUtils.getTestServerStripe(handleRequest, (err, stripe) => {
-      stripe.quotes
+      if (err) {
+        return callback(err);
+      }
+
+      return stripe.quotes
         .pdf({id: 'qt_123'}, {host: 'localhost'})
         .then((res) => {
-          return callback();
+          res.pipe(fs.createWriteStream('/tmp/myquotespdf'));
+          res.on('end', () => {
+            return callback();
+          });
         })
         .catch((err) => {
           return callback(err);
